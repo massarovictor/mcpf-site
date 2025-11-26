7850b579a3854e34a72a59cc5958e5d315edef75
GuilhermmeDev <139175554+GuilhermmeDev@users.noreply.github.com>
2025-11-26 15:55:27 -0300

    colocando component sonner

---
+++ components/ui/sonner.tsx
@@ -0,0 +1,29 @@
+import { useTheme } from "next-themes";
+import { Toaster as Sonner } from "sonner";
+
+type ToasterProps = React.ComponentProps<typeof Sonner>;
+
+const Toaster = ({ ...props }: ToasterProps) => {
+  const { theme = "system" } = useTheme();
+
+  return (
+    <Sonner
+      theme={theme as ToasterProps["theme"]}
+      className="toaster group"
+      toastOptions={{
+        classNames: {
+          toast:
+            "group toast dark:group-[.toaster]:bg-slate-900/50 group-[.toaster]:bg-slate-200/50 group-[.toaster]:text-slate-950 dark:group-[.toaster]:text-white dark:group-[.toaster]:border-slate-700 group-[.toaster]:border-slate-300 group-[.toaster]:shadow-lg",
+          description: "group-[.toast]:text-muted-foreground",
+          actionButton:
+            "group-[.toast]:bg-primary group-[.toast]:text-primary-foreground",
+          cancelButton:
+            "group-[.toast]:bg-muted group-[.toast]:text-muted-foreground",
+        },
+      }}
+      {...props}
+    />
+  );
+};
+
+export { Toaster };
