bf7346445a1ec0f0f8310fbaeecc502f6c047170
Massaro <30939659+massarovictor@users.noreply.github.com>
2025-11-28 18:46:42 -0300

    Add Vercel Analytics

--- index.tsx
+++ index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { SpeedInsights } from '@vercel/speed-insights/react';
+import { Analytics } from '@vercel/analytics/react';
 import './index.css';
 import App from './App';
 
@@ -14,5 +15,6 @@ root.render(
   <React.StrictMode>
     <App />
     <SpeedInsights />
+    <Analytics />
   </React.StrictMode>
 );
